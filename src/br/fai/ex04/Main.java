31e25a8ef131b2b06412716819fb531283eff372
mario <mariodentuo22@gmail.com>
2022-02-22 21:45:41 -0300

    Feat:finalização do projeto

--- src/br/fai/ex04/Main.java
+++ src/br/fai/ex04/Main.java
@@ -3,14 +3,40 @@ package br.fai.ex04;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Main app = new Main();
 		app.start();
 	}
-	
-	private void start() {
-		
-	}
-	
 
+	private void start() {
+
+		iterarUsandoWhile();
+		iterarUsandoDoWhile();
+		iterarUsandoFor();
+	}
+
+	private void iterarUsandoFor() {
+		for(int j = 0; j < 20; j++) {
+			System.out.println("j: " + j);
+		}
+	}
+
+	private void iterarUsandoDoWhile() {
+		int k = 0;
+		do {
+			System.out.println("k: " + k);
+			k += 1;
+		}while (k < 20);
+		System.out.println("----------");	
+	}
+
+	private void iterarUsandoWhile() {
+		int i = 0;
+		while(i < 20) {
+			System.out.println("i: "+ i);
+			i++;
+		}
+	}		
 }
+
+	
